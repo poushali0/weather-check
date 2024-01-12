21ce3900900f858c84dc3b4a19791aa1ef6d685a
Ayush Ghosal <ayush786113@gmail.com>
2024-01-13 01:20:12 +0530

    secured API key, created README

--- weather.py
+++ weather.py
@@ -1,18 +1,20 @@
+from secrets import API_KEY
 import requests
 
-API_KEY = "d65ff6691c25340dbdd075bd1eb13d04"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
-request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+request_url = "{BASE_URL}?appid={API_KEY}&q={city}".format(BASE_URL=BASE_URL, API_KEY=API_KEY, city=city)
 response = requests.get(request_url)
+data = response.json()
 
 if response.status_code == 200:
-    data = response.json()
     weather = data['weather'][0]['description']
     temperature = round(data["main"]["temp"] - 273.15, 2)
 
     print("Weather:", weather)
     print("Temperature:", temperature, "celsius")
+
 else:
-    print("An error occurred.")
+    error = data['message']
+    print(error)
